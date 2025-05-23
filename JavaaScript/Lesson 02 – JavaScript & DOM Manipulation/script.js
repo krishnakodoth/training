99f5ff723ed78149b81a0342e69a1f8f8fc23819
NSN-INTRA\krisk <krishnaprasad.1.k@nokia.com>
2025-05-23 20:02:34 +0530

    Lesson 02 – JavaScript & DOM Manipulation

---
+++ JavaaScript/Lesson 02 – JavaScript & DOM Manipulation/script.js
@@ -0,0 +1,9 @@
+const taskInput = document.getElementById("taskInput");
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskList = document.getElementById("taskList");
+
+addTaskBtn.addEventListener("click", () => {
+  const taskText = taskInput.value.trim();
+  // Add your code here
+  console.log("Adding new task:",taskText);
+});
